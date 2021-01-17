5e62d02ca0d64df454659cc3e84f6e1ccdabf320
reisyun <reisyun7@gmail.com>
2021-01-17 22:44:17 +0900

    fix: Remove nullable of media enums

--- src/infra/adapter/persistence/entity/PrismaHistoryItem.ts
+++ src/infra/adapter/persistence/entity/PrismaHistoryItem.ts
@@ -1,7 +1,6 @@
 import { HistoryItem, HistoryCategory, MediaStatus } from '@prisma/client';
-import { Nullable } from '@core/common/Types';
 
 export interface PrismaHistoryItem extends HistoryItem {
   history: { ownerId: string; category: HistoryCategory };
-  media: { status: Nullable<MediaStatus> };
+  media: { status: MediaStatus };
 }
