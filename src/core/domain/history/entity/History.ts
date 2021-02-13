dfe4d1cf0dc4329b7e2b8578a989179a7ad320c3
reisyun <reisyun7@gmail.com>
2021-02-13 21:12:45 +0900

    feat: Add rule by history status

--- src/core/domain/history/entity/History.ts
+++ src/core/domain/history/entity/History.ts
@@ -1,7 +1,10 @@
 import { IsInt, IsBoolean, IsDate, IsUUID, IsEnum, IsInstance } from 'class-validator';
 import { v4 } from 'uuid';
 import { Entity } from '@core/common/Entity';
+import { Code } from '@core/common/exception/Code';
+import { Exception } from '@core/common/exception/Exception';
 import { HistoryStatus } from '@core/common/enums/HistoryEnums';
+import { MediaStatus } from '@core/common/enums/MediaEnums';
 import { CreateHistoryEntityPayload } from '@core/domain/history/entity/type/CreateHistoryEntityPayload';
 import { EditHistoryEntityPayload } from '@core/domain/history/entity/type/EditHistoryEntityPayload';
 import { Media } from '@core/domain/history/value-object/Media';
@@ -49,6 +52,7 @@ export class History extends Entity<string> {
 
   public static async new(payload: CreateHistoryEntityPayload): Promise<History> {
     const history = new History(payload);
+    history.checkStatusRule();
     await history.validate();
 
     return history;
@@ -109,6 +113,47 @@ export class History extends Entity<string> {
     this.status = status;
     this.updatedAt = new Date();
 
+    this.checkStatusRule();
     await this.validate();
+  }
+
+  /**
+   * 기록 상태별 추가 규칙
+   *
+   * - `COMPLETED` 상태는 `FINISHED` 상태의 미디어를 추가 가능
+   * - `CURRENT` 상태는 `FINISHED`, `RELEASING` 상태의 미디어를 추가 가능
+   * - `PLANNING` 상태는 모든 상태의 미디어를 추가 가능
+   */
+  private checkStatusRule(): void {
+    if (this.getStatus === HistoryStatus.COMPLETED) {
+      this.completeStatusRule();
+    }
+    if (this.getStatus === HistoryStatus.CURRENT) {
+      this.currentStatusRule();
+    }
+  }
+
+  private completeStatusRule(): void {
+    const addableMediaStatus = this.media.getStatus === MediaStatus.FINISHED;
+
+    if (!addableMediaStatus) {
+      throw Exception.new({
+        code: Code.BAD_REQUEST_ERROR,
+        overrideMessage: `Only finished media can be added to COMPLETED status`,
+      });
+    }
+  }
+
+  private currentStatusRule(): void {
+    const addableMediaStatus =
+      this.media.getStatus === MediaStatus.FINISHED ||
+      this.media.getStatus === MediaStatus.RELEASING;
+
+    if (!addableMediaStatus) {
+      throw Exception.new({
+        code: Code.BAD_REQUEST_ERROR,
+        overrideMessage: `Only finished or releasing media can be added to CURRENT status`,
+      });
+    }
   }
 }
