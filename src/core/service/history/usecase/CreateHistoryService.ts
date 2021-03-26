3d33add5840e715bb43a5928fe3fa893b5108d34
reisyun <reisyun7@gmail.com>
2021-03-27 00:54:36 +0900

    refactor: Add exception when create history

--- src/core/service/history/usecase/CreateHistoryService.ts
+++ src/core/service/history/usecase/CreateHistoryService.ts
@@ -1,10 +1,14 @@
+import { Code } from '@core/common/exception/Code';
+import { Exception } from '@core/common/exception/Exception';
+import { CoreAssert } from '@core/common/util/CoreAssert';
+
 import { History } from '@core/domain/history/entity/History';
+import { Media } from '@core/domain/history/value-object/Media';
 
 import { HistoryRepositoryPort } from '@core/domain/history/port/persistence/HistoryRepositoryPort';
 import { CreateHistoryPort } from '@core/domain/history/port/usecase/CreateHistoryPort';
 import { HistoryUseCaseDto } from '@core/domain/history/usecase/dto/HistoryUseCaseDto';
 import { CreateHistoryUseCase } from '@core/domain/history/usecase/CreateHistoryUseCase';
-import { Media } from '@core/domain/history/value-object/Media';
 
 export class CreateHistoryService implements CreateHistoryUseCase {
   private readonly historyRepository: HistoryRepositoryPort;
@@ -18,8 +22,21 @@ export class CreateHistoryService implements CreateHistoryUseCase {
 
     // TODO: 미디어가 존재하는지 확인하기
 
-    // TODO: '기록이 이미 존재하는가?'에 따라 분기 또는 다른 방안 생각해보기
-    // 현재 merge방식으로 DB에 저장하는데, update할 경우 이전 상태를 버리고 새로운 상태를 덮어쓰는 문제가 있음
+    // create history는 update와 혼용(merge)하지 않는게 맞다고 생각함.
+    // 그러므로 이미 존재하는 history는 예외처리.
+
+    // TODO: 이 부분은 클라이언트와 연동할 때 다시 살펴봐야 할 것 같음.
+    const doesHistoryExist = !!(await this.historyRepository.count({
+      where: { AND: { userId: executorId, mediaId } },
+    }));
+
+    CoreAssert.isFalse(
+      doesHistoryExist,
+      Exception.new({
+        code: Code.ENTITY_ALREADY_EXISTS_ERROR,
+        overrideMessage: 'This history already exists.',
+      }),
+    );
 
     const history: History = await History.new({
       media: new Media(mediaId),
